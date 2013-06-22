5070640653810e686aed53fec3f2d2a739381458
Frank Carnevale <frank.carnevale@gmail.com>
2013-06-22 19:44:07 -0400

    Add new and create actions to people controller

--- app/controllers/people_controller.rb
+++ app/controllers/people_controller.rb
@@ -5,4 +5,19 @@ class PeopleController < ApplicationController
     @people = Person.all.sort_by(&:name)
   end
 
+  def new
+    @person = Person.new
+  end
+
+  def create
+    @person = Person.new(params[:person].permit(:name, :email))
+
+    if @person.save
+      flash[:success] = "Person created!"
+      redirect_to people_url
+    else
+      render 'new'
+    end
+  end
+
 end
