3565c01bb9f6c6e0706dc4af16e40e97ccaee319
Frank Carnevale <frank.carnevale@gmail.com>
2013-06-22 17:34:09 -0400

    Add name validation to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :name,  presence: true, length: { maximum: 50 }
   validates :password, length: { minimum: 6 }
   validates :password_confirmation, presence: true
 
